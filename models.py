77183a8e1abd264aa1ca234051125fa22c3104b1
Martin Buchleitner <mabunixda@gmail.com>
2020-07-03 09:18:52 +0200

    liniting...
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask import g
-from contextlib import closing
+
 
 def dict_factory(cursor, row):
     row_data = {}
@@ -8,35 +8,35 @@ def dict_factory(cursor, row):
         row_data[col[0]] = row[idx]
     return row_data
 
+def create_article_table(connection):
+    query = """
+    CREATE TABLE IF NOT EXISTS "article" (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        content TEXT,
+        _is_deleted boolean DEFAULT(false)
+    ) ;
+    """
+    connection.execute(query)
+
+def connect_db(database):
+    conn = sqlite3.connect(database)
+    conn.row_factory = dict_factory
+    init_db(conn)
+    return conn
+
+def init_db(conn):
+    create_article_table(conn)
+    conn.commit()
 
 class ArticleModel():
-    def __init__(self, db):
+    def __init__(self, database):
         if "db" not in g:
-            g.conn = self.connect_db(db)
-
-    def connect_db(self, db):
-        conn = sqlite3.connect(db)
-        conn.row_factory = dict_factory
-        self.init_db(conn)
-        return conn
-
-    def init_db(self, conn):
-        self.create_article_table(conn)
-        conn.commit()
-
-    def create_article_table(self, db):
-        query = """
-        CREATE TABLE IF NOT EXISTS "article" (
-          id INTEGER PRIMARY KEY,
-          title TEXT,
-          content TEXT,
-          _is_deleted boolean DEFAULT(false)
-        ) ;
-        """
-        db.execute(query)
+            g.conn = connect_db(database)
+        self.conn = g.conn
 
     def create(self, title, content):
-        cursor = g.conn.cursor()
+        cursor = self.conn.cursor()
         cursor.execute(
             "INSERT INTO article(title, content) VALUES(?, ?)", (title, content))
         g.conn.commit()
@@ -44,12 +44,12 @@ class ArticleModel():
         return self.get(str(last_id))
 
     def list_items(self):
-        cursor = g.conn.execute("SELECT * from article")
+        cursor = self.conn.execute("SELECT * from article")
         result = cursor.fetchall()
         return result
 
     def get(self, content_id):
-        cur = g.conn.execute(
+        cur = self.conn.execute(
             "SELECT id,title,content from article WHERE id=?", (content_id))
         result = cur.fetchone()
         return result
