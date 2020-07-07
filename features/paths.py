ad3e09e18e527fbd9b66404621af7456785ebe24
Martin Buchleitner <mabunixda@gmail.com>
2020-07-07 09:10:47 +0200

    add pydoc to run plan pylint
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- features/paths.py
+++ features/paths.py
@@ -1,7 +1,16 @@
+"""
+naviation behave module
+"""
 class NavigationHelpers():
-
+    """
+    map behave strings to uri
+    """
     def __init__(self, base_url="http://127.0.0.1:5000"):
-
+        """
+        init helper class
+        args:
+            base_url: base url of unit under test
+        """
         self.switch = {
             "the home page": (base_url + "/"),
             "the about page": base_url + "/about",
@@ -10,7 +19,22 @@ class NavigationHelpers():
             "api_article_list": base_url+"/api/article"
         }
 
+    def add_mapping(self, name, uri):
+        """
+        add mapping to helper class
+        """
+        self.switch[name] = uri
+
     def path_to(self, page_name):
+        """
+        map a page_name to an uri
+        args:
+            page_name: name of page used in behave strings
+        returns:
+            uri of page
+        raises:
+            exception if page is not found
+        """
         if not self.switch.__contains__(page_name):
             raise Exception(
                 "Can\'t find mapping from '{page_name}' - add a mapping in paths.py")
