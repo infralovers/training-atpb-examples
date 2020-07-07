5973dae91045f36fdac4e84bc8be1896e50b2206
Martin Buchleitner <mabunixda@gmail.com>
2020-07-07 09:10:47 +0200

    add pydoc to run plan pylint
    Signed-off-by: Martin Buchleitner <mabunixda@gmail.com>

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,6 @@
+"""
+behave environment configuration
+"""
 import os
 import threading
 import tempfile
@@ -14,14 +17,29 @@ DRIVER = os.getenv('DRIVER', 'firefox')
 
 
 def get_chrome_driver():
+    """get behave webdriver for chrome
+
+    Returns:
+        selenium webdriver
+    """
     return behave_webdriver.Chrome.headless()
 
 
 def get_firefox_driver():
+    """get behave webdriver for firefox
+
+    Returns:
+        selenium webdriver
+    """
     return behave_webdriver.Firefox.headless()
 
 
 def get_headless_driver():
+    """get headless behave webdriver for chrome
+
+    Returns:
+        selenium webdriver
+    """
 
     chrome_options = Options()
 # argument to switch off suid sandBox and no sandBox in Chrome
@@ -37,6 +55,11 @@ def get_headless_driver():
 
 
 def get_browser_driver():
+    """
+    get a browser based in global DRIVER variables - defaults to firefox
+    returns:
+        a selenium webdriver
+    """
     if DRIVER == "firefox":
         return get_firefox_driver()
     if DRIVER == "chrome":
@@ -45,8 +68,15 @@ def get_browser_driver():
     return get_headless_driver()
 
 
+# pylint: disable=W0613
 @fixture
 def app_client(context, *args, **kwargs):
+    """
+    fix application client setup with a temporary database, which is destroyed after the test
+    The unsued argument warning is explicit deactivated,
+    because the fixure method requires those parameters,
+    but those parameters are not used within the current implementation
+    """
     context.db, app.config['DATABASE'] = tempfile.mkstemp()
     app.testing = True
     context.client = app.test_client()
@@ -60,7 +90,12 @@ def app_client(context, *args, **kwargs):
 
 
 def before_all(context):
-
+    """
+    before all tests are started a setup for the test context is needed:
+        - run http server
+        - setup navigation helpers
+        - setup selenium webdriver
+    """
     use_fixture(app_client, context)
 
     context.server = simple_server.WSGIServer(("", 5000), WSGIRequestHandler)
@@ -74,6 +109,9 @@ def before_all(context):
 
 
 def after_all(context):
+    """
+    after testing the webdriver and webserver are closed and shutdown
+    """
     context.browser.close()
     context.server.shutdown()
     context.pa_app.join()
--- models.py
+++ models.py
@@ -1,14 +1,28 @@
+"""
+model representation of databsae object
+"""
 import sqlite3
 from flask import g
 
 
 def dict_factory(cursor, row):
+    """
+    map row data to a dictionary
+    args:
+        cursor: database cursor
+        row: current row
+    returns:
+        row representation as dictionary
+    """
     row_data = {}
     for idx, col in enumerate(cursor.description):
         row_data[col[0]] = row[idx]
     return row_data
 
 def create_article_table(connection):
+    """
+    create an article table in database
+    """
     query = """
     CREATE TABLE IF NOT EXISTS "article" (
         id INTEGER PRIMARY KEY,
@@ -20,22 +34,50 @@ def create_article_table(connection):
     connection.execute(query)
 
 def connect_db(database):
+    """
+    connect database based on argument string
+    args:
+        database: file to open as database
+    returns:
+        sqlite database connection
+    """
     conn = sqlite3.connect(database)
     conn.row_factory = dict_factory
     init_db(conn)
     return conn
 
 def init_db(conn):
+    """
+    init database and commit change
+    args:
+        conn: current connection
+    """
     create_article_table(conn)
     conn.commit()
 
 class ArticleModel():
+    """
+    Article Model to interact with database
+    """
     def __init__(self, database):
+        """
+        init model and database
+        args:
+            database: database file to open as sqlite
+        """
         if "db" not in g:
             g.conn = connect_db(database)
         self.conn = g.conn
 
     def create(self, title, content):
+        """
+        create an article
+        args:
+            title: title of article
+            content: content of article
+        returns:
+            database represenstation of article
+        """
         cursor = self.conn.cursor()
         cursor.execute(
             "INSERT INTO article(title, content) VALUES(?, ?)", (title, content))
@@ -44,11 +86,23 @@ class ArticleModel():
         return self.get(str(last_id))
 
     def list_items(self):
+        """
+        list all articles
+        returns:
+            list of articles
+        """
         cursor = self.conn.execute("SELECT * from article")
         result = cursor.fetchall()
         return result
 
     def get(self, content_id):
+        """
+        get article based on content_id
+        args:
+            content_id: id of article
+        returns:
+            article from database
+        """
         cur = self.conn.execute(
             "SELECT id,title,content from article WHERE id=?", (content_id))
         result = cur.fetchone()
