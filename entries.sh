73b266912d618195dc64b762808f5f48627fe0e9
Martin Buchleitner <mabunixda@gmail.com>
2021-02-04 09:37:38 +0100

    refactor: port as env variable to adapt port easier

--- entries.sh
+++ entries.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My first entry", "content": "This is my first blog entry"}' '127.0.0.1:5000/api/article'
-curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My second entry", "content": "This is my second blog entry"}' '127.0.0.1:5000/api/article'
-curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My third entry", "content": "This is my third blog entry"}' '127.0.0.1:5000/api/article'
+PORT=${PORT:-5000}
+
+curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My first entry", "content": "This is my first blog entry"}' 'http://127.0.0.1:${PORT}/api/article'
+curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My second entry", "content": "This is my second blog entry"}' 'http://127.0.0.1:${PORT}/api/article'
+curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My third entry", "content": "This is my third blog entry"}' 'http://127.0.0.1:${PORT}/api/article'
