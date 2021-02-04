810105a139b2aacf71df0e93815ec801e8c21012
Martin Buchleitner <mabunixda@gmail.com>
2021-02-04 10:49:57 +0100

    refactor: also do request on endpoint

--- entries.sh
+++ entries.sh
@@ -2,6 +2,9 @@
 
 PORT=${PORT:-5000}
 
-curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My first entry", "content": "This is my first blog entry"}' 'http://127.0.0.1:${PORT}/api/article'
-curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My second entry", "content": "This is my second blog entry"}' 'http://127.0.0.1:${PORT}/api/article'
-curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My third entry", "content": "This is my third blog entry"}' 'http://127.0.0.1:${PORT}/api/article'
+curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My first entry", "content": "This is my first blog entry"}' "http://127.0.0.1:${PORT}/api/article"
+curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My second entry", "content": "This is my second blog entry"}' "http://127.0.0.1:${PORT}/api/article"
+curl -X POST --header 'Content-Type: application/json' -d '{ "title": "My third entry", "content": "This is my third blog entry"}' "http://127.0.0.1:${PORT}/api/article"
+
+
+curl --header 'Content-Type: application/json' "http://127.0.0.1:${PORT}/api/article"
