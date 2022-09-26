f667c644e6eb809760e6df694add6c0ea645989e
Martin Buchleitner <mabunixda@gmail.com>
2022-09-26 09:07:59 +0200

    backport changes

--- features/steps/about_page_steps.py
+++ features/steps/about_page_steps.py
@@ -1,4 +1,5 @@
 from behave import when, given, then
+from selenium.webdriver.common.by import By
 
 @given(u'I am on "{page_name}"')
 def step_impl(context, page_name):
@@ -8,14 +9,8 @@ def step_impl(context, page_name):
 
 @when(u'I follow "{link_text}"')
 def step_impl(context, link_text):
-    link = context.browser.find_element_by_link_text(link_text)
-    assert link != None, "Was not able to find %s" % link_text
-    print("link url: {}".format(link.get_attribute("href")))
-    ## as link.click() does not work always on headless:
+    link = context.browser.find_element(By.LINK_TEXT, link_text)
     link.click()
-    ## there are workarounds "available":
-    # context.browser.execute_script("arguments[0].click();", link)
-    # context.browser.get(link.get_attribute("href"))
 
 @then(u'I should be on "{page_name}"')
 def step_impl(context, page_name):
